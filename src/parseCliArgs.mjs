7a8d14a6c6065a41072ffbdb8a58ef8cd3d8a285
Ricardo C <rmec1996@gmail.com>
2025-05-31 16:49:19 +0100

    parseCliArgs.mjs: Added support for caseSensitive config. Added args processor

--- src/parseCliArgs.mjs
+++ src/parseCliArgs.mjs
@@ -77,14 +77,17 @@ function compileCliOptions({ options = {}, mappings = {}, caseSensitive = true, 
   /** @type {ProcessedOptionMap} */
   const processedOptionMap = {}
 
-  for (const [name, opc] of Object.entries(options)) {
-    //TODO check and convent the name HERE,simply force it to lowercase here
+  for (let [name, opc] of Object.entries(options)) {
     /** @type {Option} */
     const val = typeof opc === "string" ? { type: opc } : opc;
 
     const type = val.type ? val.type : "String"
 
-    const alias = val.alias ? val.alias instanceof Array ? [name, ...val.alias] : [name, val.alias] : [name]
+    let alias = val.alias ? val.alias instanceof Array ? [name, ...val.alias] : [name, val.alias] : [name]
+    if (!caseSensitive) {
+      alias = alias.map(a => a.toLocaleLowerCase())
+      name = alias[0];
+    }
     const seperator = typeof val.seperator === "string" ? val.seperator : ",";
     const min = typeof val.min === "number" ? val.min : null;
     const max = typeof val.max === "number" ? val.max : null;
@@ -112,8 +115,6 @@ function compileCliOptions({ options = {}, mappings = {}, caseSensitive = true, 
     }
 
     for (let a of alias) {
-      //TODO have a check that the name and aliases aren't being repeated between options
-      //TODO make that check be case aware
       if (processedOptionMap[a]) {
         throw new Error(`'${a}' is already used by option/mapping '${processedOptionMap[a].name}'`)
       } else {
@@ -125,20 +126,22 @@ function compileCliOptions({ options = {}, mappings = {}, caseSensitive = true, 
   }
 
   //process the mappings here
-  for (const [name, mapping] of Object.entries(mappings)) {
-    //TODO have a check that the name and aliases aren't being repeated between options and mappings
-    //TODO make that check be case aware
-    const alias = mapping.alias ? mapping.alias instanceof Array ? [name, ...mapping.alias] : [name, mapping.alias] : [name]
+  for (let [name, mapping] of Object.entries(mappings)) {
+    let alias = mapping.alias ? mapping.alias instanceof Array ? [name, ...mapping.alias] : [name, mapping.alias] : [name]
+    if (!caseSensitive) {
+      alias = alias.map(a => a.toLocaleLowerCase())
+      name = alias[0];
+    }
     const description = generateDescription(prefix, alias, mapping.description, "Mapping", null, null, "");
+    const option = caseSensitive ? mapping.option : mapping.option.toLocaleLowerCase();
 
-    //TODO check that the value is valid for the respective option, if none exist (aka its a "fake" option) the value can be anything
-    // Object.hasOwn(baseResult,mapping.option)
+    if (!Object.hasOwn(baseResult, option)) {
+      baseResult[option] = null
+    }
 
     /** @type {MappingOption} */
-    let processedOption = { name, type: "Mapping", option: mapping.option, description, value: mapping.value };
+    let processedOption = { name, type: "Mapping", option, description, value: mapping.value };
     for (let a of alias) {
-      //TODO have a check that the name and aliases aren't being repeated between options
-      //TODO make that check be case aware
       if (processedOptionMap[a]) {
         throw new Error(`'${a}' is already used by option/mapping '${processedOptionMap[a].name}'`)
       } else {
@@ -147,7 +150,140 @@ function compileCliOptions({ options = {}, mappings = {}, caseSensitive = true, 
     }
   }
 
+  //Checks if there isn't any imcompatibility between the argsList and any options
+  if (Object.hasOwn(baseResult, argList)) {
+    throw new Error(`The given argsList value '${argList}' is used by an option/mapping already.`)
+  } else {
+    baseResult[argList] = [];
+  }
+
   return { options: processedOptionMap, caseSensitive, failUnknowFlags, prefix, setter, hifenToCamel, baseResult, argList }
+}
+
+/**
+ * 
+ * @param {CompiledCliConfig} config
+ * @param {string[]} args
+ */
+function processCliArguments(config, args) {
+  let result = structuredClone(config.baseResult)
+  args = structuredClone(args);
+  while (args.length) {
+    let curr = args.shift();
+    if (curr?.startsWith(config.prefix)) {
+      let value;
+      curr = curr.slice(config.prefix.length);
+      const setterPos = config.setter ? curr.indexOf(config.setter) : -1;
+
+      //is the setter format
+      if (setterPos !== -1) {
+        value = curr.slice(setterPos + config.setter.length);
+        curr = curr.slice(0, setterPos);
+      }
+
+      if (!config.caseSensitive) {
+        curr = curr.toLocaleLowerCase();
+      }
+
+      if (config.options[curr]) {
+        //in processing check if is flag/mapping or another
+        // if flag mapping check that the values wasnt set, if it was fail because you can't set a flag/mapping value, if not process is as expected for the flag or mapping
+        // for the other types if the value hasnt been set, check that one exists oin the args and set it as the value to process, else throw an error
+        // after that if a value exist just proces it depending on the type and do the necessary checks
+        const opc = config.options[curr];
+        if (opc.type === "Flag" || opc.type === "Mapping") {
+          if (value !== undefined) {
+            throw new Error(`${config.prefix}${curr} is a ${opc.type} option meaning it can't have a value set to it by using ${config.setter}.`);
+          }
+          if (opc.type == "Flag") {
+            result[opc.name] = true;
+          } else {
+            result[opc.option] = opc.value;
+          }
+        } else {
+          if (value === undefined) {
+            if (args.length) {
+              value = args.shift();
+            } else {
+              throw new Error(`No value given for ${config.prefix}${curr}`);
+            }
+          }
+
+          switch (opc.type) {
+            case "String":
+              if (opc.min !== null) {
+                if (value?.length < opc.min) {
+                  throw new Error(`${config.prefix}${curr} value must be at least ${opc.min} characters long.`);
+                }
+              }
+              if (opc.max !== null) {
+                if (value?.length < opc.max) {
+                  throw new Error(`${config.prefix}${curr} value must not be over ${opc.max} characters long.`);
+                }
+              }
+              break;
+            case "Number":
+              value = Number(value);
+              if (Number.isNaN(value)) {
+                throw new Error(`${config.prefix}${curr} value must be a number.`);
+              }
+              if (opc.min !== null) {
+                if (value < opc.min) {
+                  throw new Error(`${config.prefix}${curr} value must be at least ${opc.min}.`);
+                }
+              }
+              if (opc.max !== null) {
+                if (value < opc.max) {
+                  throw new Error(`${config.prefix}${curr} value must not be over ${opc.max}.`);
+                }
+              }
+              break;
+            case "Integer":
+              value = Number(value);
+              if (Number.isNaN(value) || Math.trunc(value) !== value) {
+                throw new Error(`${config.prefix}${curr} value must be an integer.`);
+              }
+              if (opc.min !== null) {
+                if (value < opc.min) {
+                  throw new Error(`${config.prefix}${curr} value must be at least ${opc.min}.`);
+                }
+              }
+              if (opc.max !== null) {
+                if (value < opc.max) {
+                  throw new Error(`${config.prefix}${curr} value must not be over ${opc.max}.`);
+                }
+              }
+              break;
+            case "List":
+              value = value.split(opc.seperator);
+              if (opc.min !== null) {
+                if (value?.length < opc.min) {
+                  throw new Error(`${config.prefix}${curr} list must have at least ${opc.min} values.`);
+                }
+              }
+              if (opc.max !== null) {
+                if (value?.length < opc.max) {
+                  throw new Error(`${config.prefix}${curr} list must not hvae over ${opc.max} values.`);
+                }
+              }
+              break;
+            case "Bool":
+              value = value.toLocaleLowerCase();
+              value = value === "true" || value === "yes" || value === "y" || (!Number.isNaN(Number(value)) && Number(value) !== 0)//any non zero number will be considered as a true value
+              break;
+          }
+
+          result[opc.name] = value;
+        }
+      } else {
+        throw new Error(`${config.prefix}${curr} is not a valid option.`)
+      }
+
+    } else {
+      result[config.argList].push(curr);
+    }
+  }
+  return result;
 }
 
 
@@ -161,12 +297,14 @@ function parseCliArgs(config = {}) {
 
   console.dir(compiledConfig, { depth: 5 })
 
-  const result = {};
+  const result = processCliArguments(compiledConfig, args);
   //check and process the actual args
+  console.log(result)
 }
 
 
 /**
+ * make the system use the default values
  * setter string that defines the value of the option, e.g. setter '='  --test=123 would convert to {test:"123"} 
  * most values are initialized to null, except the Flag type, that is set to false
  * argList is the name of the parameter that stores all non option arguments
@@ -180,7 +318,9 @@ function parseCliArgs(config = {}) {
  * but it might work as long as the help text generator is aware that it should only work like that only for flags and mappings and the same for the system that will process the input
 
  * have a parameter to fail on non option args or make it so that is defined by the setting or not of the argList parameter
+
+tecnically the prefix and setter also need to be converted if is set to case insensitive or to keep it simple (aka what is going to happen) consider that those are ALWAYS case sensitive
  */
 
 
-parseCliArgs({ options: { test: { alias: ["T"], type: "Number", description: "test flag" } }, mappings: { one: { option: "test", value: 1 } } })
+parseCliArgs({ options: { test: { alias: ["T"], type: "Number", description: "test flag" } }, mappings: { one: { option: "test", value: 1, alias: "t" } } })
